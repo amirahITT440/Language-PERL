e525c01c7c09e09b2ab7f63a73bdefee2dd901ce
amirahITT440 <mirakhairi95@yahoo.com>
2016-04-05 17:39:35 +0800

    The server and client side using TCP in PERL

---
+++ TCPclient.pl
@@ -0,0 +1,23 @@
+use IO::Socket::INET;
+
+$| = 1;
+
+my $socket = new IO::Socket::INET(
+	PeerHost => '192.168.1.0',
+	PeerPort => '7890',
+	Proto => 'tcp',
+);
+die "Cannot connect to the server $!\n" unless $socket;
+print "Connected to the server\n";
+
+my $req = 'Hello World';
+my $size = $socket->send($req);
+print "Sent data of length $size\n";
+
+shutdown($socket, 1);
+
+my $response = "";
+$socket->recv($response, 1024);
+print "Received response: $response\n";
+
+$socket->close();
---
+++ TCPserver.pl
@@ -0,0 +1,33 @@
+use IO::Socket::INET;
+
+$| = 1;
+
+my $socket = new IO::Socket::INET(
+	LocalHost => '0.0.0.0',
+	LocalPort => '7890',
+	Proto => 'tcp',
+	Listen => 5,
+	Reuse => 1
+);
+die "Cannot create socket $!\n" unless $socket;
+print "Server waiting for client connection on port 7890\n";
+
+while(1)
+{
+	my $client_socket = $socket->accept();
+	
+	my $client_address = $client_socket->peerhost();
+	my $client_port = $client_socket->peerport();
+	print "Connection from $client_address:$client_port\n";
+
+	my $data = "";
+	$client_socket->recv($data,1024);
+	print "Received data : $data\n";
+
+	$data = "OK";
+	$client_socket->send($data);
+
+	shutdown($client_socket, 1);
+}
+
+$socket->close();
